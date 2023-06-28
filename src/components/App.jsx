e8eb96fd553e07947abaed1e824afff78c6a24b1
Alexander Klymuk <swcAlexander@gmail.com>
2023-06-28 20:07:27 +0300

    add modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,91 @@
-export const App = () => {
-  
+import React from 'react';
+import Modal from './Modal/Modal';
 
-    searchQuery = '';
-    page = 1;
-    PER_PAGE = 40;
+export class App extends React.Component {
+  state = {
+    showModal: false,
+  };
+  // searchQuery = '';
+  // page = 1;
+  // PER_PAGE = 40;
 
-    async fetchGallery =()=> {
-    const axiosOptions = {
-      method: 'get',
-      url: 'https://pixabay.com/api/',
-      params: {
-        key: '35072085-a0b1b3afc3e4ed85b172a35ba',
-        q: `${this.searchQuery}`,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        page: `${this.page}`,
-        per_page: `${this.PER_PAGE}`,
-      },
-    };
-    try {
-      const response = await axios(axiosOptions);
+  // const fetchGallery = () => {
+  //   const axiosOptions = {
+  //     method: 'get',
+  //     url: 'https://pixabay.com/api/',
+  //     params: {
+  //       key: '35072085-a0b1b3afc3e4ed85b172a35ba',
+  //       q: `${this.searchQuery}`,
+  //       image_type: 'photo',
+  //       orientation: 'horizontal',
+  //       safesearch: true,
+  //       page: `${this.page}`,
+  //       per_page: `${this.PER_PAGE}`,
+  //     },
+  //   };
+  //   try {
+  //     const response = axios(axiosOptions);
 
-      this.incrementPage();
-      return response.data;
-    } catch {
-      console.log('Помилка!');
-    }
+  //     this.incrementPage();
+  //     return response.data;
+  //   } catch {
+  //     console.log('Помилка!');
+  //   }
+  // };
+
+  // incrementPage = () => {
+  //   this.page += 1;
+  // };
+
+  // resetPage = () => {
+  //   this.page = 1;
+  // };
+
+  // resetEndOfHits = () => {
+  //   this.endOfHits = false;
+  // };
+  // hasMoreImages = () => {
+  //   return this.page === Math.ceil(this.totalPages / this.per_page);
+  // };
+
+  // getquery = () => {
+  //   return this.searchQuery;
+  // };
+
+  // setquery = newQuery => {
+  //   this.searchQuery = newQuery;
+  // };
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  render() {
+    const { showModal } = this.state;
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <button type="buton" onClick={this.toggleModal}>
+          open modal
+        </button>
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <img src="" alt="" />
+            <button type="buton" onClick={this.toggleModal}>
+              close modal
+            </button>
+          </Modal>
+        )}
+      </div>
+    );
   }
-
-  incrementPage =() => {
-    this.page += 1;
-  }
-
-  resetPage=() => {
-    this.page = 1;
-  }
-
-  resetEndOfHits=() => {
-    this.endOfHits = false;
-  }
-  hasMoreImages=() => {
-    return this.page === Math.ceil(this.totalPages / this.per_page);
-  }
-
-  getquery=() => {
-    return this.searchQuery;
-  }
-
-  setquery=(newQuery)=> {
-    this.searchQuery = newQuery;
-  }
-
-
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+}
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React from 'react';
+import { createPortal } from 'react-dom';
+import styles from 'components/Modal/Modal.module.css';
 
+const modalRoot = document.querySelector('#modal-root');
 
-const Modal = () => {
-    return (
-        <div class="overlay">
-            <div class="modal">
-                <img src="" alt="" />
-            </div>
+export default class Modal extends React.Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.hendleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.hendleKeyDown);
+  }
+  hendleKeyDown = e => {
+    if (e.code === 'Escape') this.props.onClose();
+  };
+  handleBackdropClick = e => {
+    if (e.currentTarget === e.target) this.props.onClose();
+  };
+  render() {
+    return createPortal(
+      <div className={styles.overlay} onClick={this.handleBackdropClick}>
+        <div className={styles.modal}>
+          {this.props.children}
+          {/* <img src="" alt="" /> */}
         </div>
-    )
+      </div>,
+      modalRoot
+    );
+  }
 }
